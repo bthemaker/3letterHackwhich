ebe363b1938ef969c88a9c990aa7497ec049ef1c
Bruno Garcia <bthemaker@gmail.com>
2021-07-26 13:37:04 -0400

    Initial Commit

---
+++ 3letterHackwhich/_letterHackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  _letterHackwhichApp.swift
+//  3letterHackwhich
+//
+//  Created by Bruno Garcia on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct _letterHackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
